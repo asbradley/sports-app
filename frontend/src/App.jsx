ce8719423d4bfcc16372765591e1bb4320701e94
aidanbradley <bradl154@purdue.edu>
2025-05-11 14:02:37 -0400

    I dont even know

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TestComponent from './components/test';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <h1>My React App</h1>
       <TestComponent />
+
     </div>
   );
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,4 +1,5 @@
 import App from "./App"
+import React from "react";
 
 import { createRoot } from "react-dom/client";
 
@@ -6,4 +7,4 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
     <App />
-);
+); 
